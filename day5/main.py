e94abcf277cba8a8bfdad2167d5960676d2f6927
Nick Kanel <nickolas.kanel@accenture.com>
2019-12-05 17:02:06 -0500

    day 5 part 1 complete

---
+++ day5/main.py
@@ -0,0 +1,117 @@
+import time
+memory = dict()
+
+def main():
+    data = ''
+    with open('input', 'r') as f:
+        data = f.read()
+    codes = list(map(lambda x: int(x), data.split(',')))
+
+    init_memory(codes)
+
+    compute()
+
+def compute():
+    index = 0
+    while memory[index] != 99:
+        # print(memory[index])
+        # time.sleep(0.05)
+        if get_op_code(memory[index]) == 1:
+            param0 = get_op_code_param(memory[index], 0)
+            param1 = get_op_code_param(memory[index], 1)
+            param2 = get_op_code_param(memory[index], 2)
+
+            input0 = None
+            input1 = None
+            input2 = None
+
+            if param0 == 0:
+                input0 = memory[memory[index+1]]
+            else:
+                input0 = memory[index+1]
+
+            if param1 == 0:
+                input1 = memory[memory[index+2]]
+            else:
+                input1 = memory[index+2]
+
+            if param2 == 0:
+                input2 = memory[index+3]
+            else:
+                print('THIS SHOULD NEVER HAPPEN FOR ADD')
+                input2 = memory[index+3]
+
+            add(input0, input1, input2)
+            index += 4
+        if get_op_code(memory[index]) == 2:
+            param0 = get_op_code_param(memory[index], 0)
+            param1 = get_op_code_param(memory[index], 1)
+            param2 = get_op_code_param(memory[index], 2)
+
+            input0 = None
+            input1 = None
+            input2 = None
+
+            if param0 == 0:
+                input0 = memory[memory[index+1]]
+            else:
+                input0 = memory[index+1]
+
+            if param1 == 0:
+                input1 = memory[memory[index+2]]
+            else:
+                input1 = memory[index+2]
+
+            if param2 == 0:
+                input2 = memory[index+3]
+            else:
+                print('THIS SHOULD NEVER HAPPEN FOR MULTIPLY')
+                input2 = memory[index+3]
+
+            multiply(input0, input1, input2)
+            index += 4
+        if get_op_code(memory[index]) == 3:
+            read_value(memory[index+1])
+            index += 2
+        if get_op_code(memory[index]) == 4:
+            print_value(memory[index+1])
+            index += 2
+    # return memory[0]
+
+def left_pad(string, length, pad = '0'):
+    if len(string) >= length:
+        return string
+    return ''.join([pad]*(length-len(string))) + string
+
+def get_op_code(code):
+    return int(left_pad(str(code), 2)[-2:])
+
+def get_op_code_param(code, index):
+    padded_code = left_pad(str(code), 2 + index + 1)
+    params = padded_code[:len(padded_code)-2]
+    return int(params[len(params) - 1 - index])
+
+def init_memory(codes):
+    global memory
+    memory = dict()
+    for index, code in enumerate(codes):
+        memory[index] = code
+
+def get_input():
+    return 1
+
+def print_value(pos):
+    print(memory[pos])
+
+def read_value(pos):
+    memory[pos] = get_input()
+
+def add(input1, input2, pos3):
+    memory[pos3] = input1 + input2
+
+def multiply(input1, input2, pos3):
+    memory[pos3] = input1 * input2
+    
+
+if __name__ == '__main__':
+    main()
