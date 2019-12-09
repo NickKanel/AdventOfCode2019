e9aafb6b6b35e94856ea80f54f5c2ad14d696647
Nick Kanel <nickolas.kanel@accenture.com>
2019-12-09 01:40:30 -0500

    .

---
+++ day9/helpers.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"strconv"
+)
+
+func leftPad(s string, l int, c rune) string {
+	if len(s) >= l {
+		return s
+	}
+
+	sLen := len(s)
+	for i := 0; i < (l - sLen); i++ {
+		s = string(c) + s
+	}
+
+	return s
+}
+
+func getOpCode(code int) int {
+	paddedCode := leftPad(strconv.Itoa(code), 2, rune(48))
+	intVal, err := strconv.Atoi(paddedCode[len(paddedCode) - 2:])
+	check(err)
+	return intVal
+}
+
+func getOpCodeParam(code int, index int) int {
+	paddedCode := leftPad(strconv.Itoa(code), 2 + index + 1, rune(48))
+	params := paddedCode[:len(paddedCode) - 2]
+	intVal, err := strconv.Atoi(string([]rune(paddedCode)[len(params) - 1 - index]))
+	check(err)
+	return intVal
+}
+
+func mapHasKey(m map[int]int, k int) bool {
+	_, ok := m[k]
+	return ok
+}
---
+++ day9/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+    "strings"
+    "strconv"
+    "io/ioutil"
+    prmt "github.com/gitchander/permutation"
+    // "sync"
+    "log"
+)
+
+var amps []*Amplifier
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
+
+func parseCodes(s string) []int {
+    intCodes := make([]int, 0)
+    splitString := strings.Split(s, ",")
+    for _, string := range splitString {
+        intValue, err := strconv.Atoi(string)
+        check(err)
+        intCodes = append(intCodes, intValue)
+    }
+    return intCodes
+}
+
+func main() {
+    fileBytes, err := ioutil.ReadFile("input")
+    check(err)
+
+    codes := parseCodes(string(fileBytes))
+
+    // maxOut := 0
+    // perms := makePerms(5, 9)
+
+
+    // var wg sync.WaitGroup
+    amp := NewAmplifier(codes, 0)
+    amp.AddInput(1)
+    amp.Compute()
+    for value := range amp.stdout {
+        log.Println(value)
+    }
+    // output := <- amp.stdout
+
+
+
+    // for _, perm := range perms {
+    //     amps = make([]*Amplifier, 0)
+    //     for i := 0; i < 5; i += 1 {
+    //         amp := NewAmplifier(codes, i, perm[i])
+    //         amps = append(amps, &amp)
+    //     }
+
+    //     amps[0].AddInput(0)
+
+    //     var wg sync.WaitGroup
+    //     for _, amp := range amps {
+    //         wg.Add(1)
+    //         go amp.Compute(&wg)
+    //     }
+    //     wg.Wait()
+
+    //     amp5Out := <- amps[0].stdin
+
+    //     if amp5Out > maxOut {
+    //         maxOut = amp5Out
+    //     }
+    // }
+
+    // log.Println(output)
+}
+
+func makePerms(start int, stop int) [][]int {
+    var perms [][]int
+
+    stop += 1
+    var elements []int
+    for i := start; i < stop; i++ {
+        elements = append(elements, i)
+    }
+
+    p := prmt.New(prmt.IntSlice(elements))
+    for p.Next() {
+        tmp := make([]int, len(elements))
+        copy(tmp, elements)
+        perms = append(perms, tmp)
+    }
+
+    return perms
+}
